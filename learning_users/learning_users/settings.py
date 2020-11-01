d377ca4d2052775babc5ce5b306aa01843114a8e
Suleiman99Hesham <suleimanhesham99@gmail.com>
2020-11-01 22:56:19 +0000

    modifications after production

--- learning_users/learning_users/settings.py
+++ learning_users/learning_users/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '#grby)v%muk(tig&azocfmgqk-!_(l48cpr&87-jennh^0bf5+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['suleimanhesham.pythonanywhere.com']
 
 
 # Application definition
